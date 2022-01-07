2831a079edcbbfb4181d81b91e0c0e30f90e506a
Tommy Lohil <tommylohil23052000@gmail.com>
2022-01-07 18:05:01 +0700

    Updated Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="5"> <Tabs /></v-col>
+        <v-col cols="12" md="7"> <Plane /></v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Plane from "@/components/Plane.vue";
+import Tabs from "@/components/Tabs.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Plane,
+    Tabs,
+  },
+};
 </script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1400px !important;
+}
+</style>
