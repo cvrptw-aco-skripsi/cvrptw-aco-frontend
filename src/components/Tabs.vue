cb6d34a8db5a4d03ed493ff63dfe575cadcb5d1a
Tommy Lohil <tommylohil23052000@gmail.com>
2022-01-07 18:07:31 +0700

    Added Tab.vue

---
+++ src/components/Tabs.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card class="outer-card">
+    <v-tabs v-model="activeTab" centered background-color="teal lighten-5" color="blue-grey darken-3">
+      <v-tab class="tab-max-width" v-for="tab in tabs" :key="tab.id">{{ tab.name }}</v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="activeTab">
+      <v-tab-item>
+        <CreateFranchisee />
+        <FranchiseeList />
+      </v-tab-item>
+      <v-tab-item>
+        <CreateTimeWindow />
+        <TimeWindowList />
+      </v-tab-item>
+    </v-tabs-items>
+  </v-card>
+</template>
+
+<script>
+import CreateFranchisee from "@/components/CreateFranchisee.vue";
+import CreateTimeWindow from "@/components/CreateTimeWindow.vue";
+import FranchiseeList from "@/components/FranchiseeList.vue";
+import TimeWindowList from "@/components/TimeWindowList.vue";
+
+export default {
+  name: "Tabs",
+  components: {
+    CreateFranchisee,
+    CreateTimeWindow,
+    FranchiseeList,
+    TimeWindowList,
+  },
+  props: {},
+  data() {
+    return {
+      activeTab: 0,
+      tabs: [
+        { id: 1, name: "Franchisees" },
+        { id: 0, name: "Time Windows" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.tab-max-width {
+  width: 100%;
+}
+</style>
