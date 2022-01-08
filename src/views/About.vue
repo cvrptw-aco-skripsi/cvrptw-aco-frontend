7a9cd1f46a8e95361c78330c8de42413ed989bef
Tommy Lohil <tommylohil23052000@gmail.com>
2022-01-08 19:28:33 +0700

    Added About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <v-card max-width="350" class="mx-auto my-3" elevation="6">
+      <img src="../../public/img/cvrp.png" alt="cvrp" width="350" />
+    </v-card>
+    <h3 style="margin-bottom: 30px">
+      OPTIMASI CAPACITATED VEHICLE ROUTING PROBLEM WITH TIME WINDOWS DALAM PENDISTRIBUSIAN PRODUK FRANCHISOR KE
+      FRANCHISEES DENGAN ALGORITMA ANT COLONY OPTIMIZATION
+    </h3>
+    <h3 style="margin-bottom: 10px">SKRIPSI</h3>
+    <h3>TOMMY LOHIL</h3>
+    <h3 style="margin-bottom: 40px">181401067</h3>
+
+    <v-btn color="green darken-1 white--text" large style="margin-bottom: 70px" :to="{ name: 'Home' }"
+      >Start Simulation</v-btn
+    >
+    <v-divider style="margin-bottom: 10px"></v-divider>
+    <v-row>
+      <v-col cols="4"> <img src="../../public/img/usu.jpg" alt="usu" width="130" /> </v-col>
+      <v-col cols="8" style="text-align: left">
+        <h4>PROGRAM STUDI S1 ILMU KOMPUTER</h4>
+        <h4>FAKULTAS ILMU KOMPUTER DAN TEKNOLOGI INFORMASI</h4>
+        <h4>UNIVERSITAS SUMATERA UTARA</h4>
+        <h4>MEDAN</h4>
+        <h4>2021</h4>
+      </v-col>
+    </v-row>
+    <h1></h1>
   </div>
 </template>
+
+<style scoped lang="scss">
+.container {
+  max-width: 800px !important;
+  text-align: center;
+}
+img {
+  border-radius: 10%;
+}
+</style>
